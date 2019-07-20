8bade8e8e31c5d782f6c3b33cde96d048a5f1585
Γιώργος Τσιάγκης <barabakos@hotmail.com>
2019-07-20 20:03:22 +0300

    Updates

---
+++ precache-manifest.4846d4627bc14933824aae569b119db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd3cfeb63fd8b25bfd3ab3501fa4f30",
+    "url": "/monsters-inc/index.html"
+  },
+  {
+    "revision": "4adc4e1279c58a3a35d1",
+    "url": "/monsters-inc/static/css/main.337c8604.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-inc/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4adc4e1279c58a3a35d1",
+    "url": "/monsters-inc/static/js/main.6be05b1b.chunk.js"
+  },
+  {
+    "revision": "ba3409997aeea374dd25",
+    "url": "/monsters-inc/static/js/runtime~main.412e6c78.js"
+  }
+]);
